38b3d242987bcc4d7bd9d47ebfdc40a35047aa83
Xiaoquan006Zhao <1009642031@qq.com>
2024-02-08 20:59:08 -0600

    update train.py

--- v1/test.py
+++ v1/test.py
@@ -34,7 +34,7 @@ WEIGHT_DECAY = 0
 EPOCHS = 1000
 NUM_WORKERS = 2
 PIN_MEMORY = True
-LOAD_MODEL = False
+LOAD_MODEL = True
 LOAD_MODEL_FILE = "overfit.pth.tar"
 IMG_DIR = "data/images"
 LABEL_DIR = "data/labels"
@@ -113,15 +113,15 @@ def main():
     )
 
     for epoch in range(EPOCHS):
-        # for x, y in train_loader:
-        #    x = x.to(DEVICE)
-        #    for idx in range(8):
-        #        bboxes = cellboxes_to_boxes(model(x))
-        #        bboxes = non_max_suppression(bboxes[idx], iou_threshold=0.5, threshold=0.4, box_format="midpoint")
-        #        plot_image(x[idx].permute(1,2,0).to("cpu"), bboxes)
+        for x, y in train_loader:
+           x = x.to(DEVICE)
+           for idx in range(8):
+               bboxes = cellboxes_to_boxes(model(x))
+               bboxes = non_max_suppression(bboxes[idx], iou_threshold=0.5, threshold=0.4, box_format="midpoint")
+               plot_image(x[idx].permute(1,2,0).to("cpu"), bboxes)
 
-        #    import sys
-        #    sys.exit()
+           import sys
+           sys.exit()
 
         pred_boxes, target_boxes = get_bboxes(
             train_loader, model, iou_threshold=0.5, threshold=0.4
@@ -132,14 +132,14 @@ def main():
         )
         print(f"Train mAP: {mean_avg_prec}")
 
-        if mean_avg_prec > 0.9:
-           checkpoint = {
-               "state_dict": model.state_dict(),
-               "optimizer": optimizer.state_dict(),
-           }
-           save_checkpoint(checkpoint, filename=LOAD_MODEL_FILE)
-           import time
-           time.sleep(10)
+        #if mean_avg_prec > 0.9:
+        #    checkpoint = {
+        #        "state_dict": model.state_dict(),
+        #        "optimizer": optimizer.state_dict(),
+        #    }
+        #    save_checkpoint(checkpoint, filename=LOAD_MODEL_FILE)
+        #    import time
+        #    time.sleep(10)
 
         train_fn(train_loader, model, optimizer, loss_fn)
 
