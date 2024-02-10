0388e653ceaa3e6e2161cd5195642d6af1d90f16
Xiaoquan006Zhao <1009642031@qq.com>
2024-02-10 02:10:20 -0600

    update train.py

--- v3/train.py
+++ v3/train.py
@@ -29,29 +29,29 @@ loader = torch.utils.data.DataLoader(
 	shuffle=True, 
 ) 
 
-# Defining the grid size and the scaled anchors 
-GRID_SIZE = [13, 26, 52] 
-scaled_anchors = torch.tensor(config.ANCHORS) / ( 
-	1 / torch.tensor(GRID_SIZE).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2) 
-) 
+# # Defining the grid size and the scaled anchors 
+# GRID_SIZE = [13, 26, 52] 
+# scaled_anchors = torch.tensor(config.ANCHORS) / ( 
+# 	1 / torch.tensor(GRID_SIZE).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2) 
+# ) 
 
-# Getting a batch from the dataloader 
-x, y = next(iter(loader)) 
+# # Getting a batch from the dataloader 
+# x, y = next(iter(loader)) 
 
-# Getting the boxes coordinates from the labels 
-# and converting them into bounding boxes without scaling 
-boxes = [] 
-for i in range(y[0].shape[1]): 
-	anchor = scaled_anchors[i] 
-	boxes += config.convert_cells_to_bboxes( 
-			y[i], is_predictions=False, s=y[i].shape[2], anchors=anchor 
-			)[0] 
+# # Getting the boxes coordinates from the labels 
+# # and converting them into bounding boxes without scaling 
+# boxes = [] 
+# for i in range(y[0].shape[1]): 
+# 	anchor = scaled_anchors[i] 
+# 	boxes += config.convert_cells_to_bboxes( 
+# 			y[i], is_predictions=False, s=y[i].shape[2], anchors=anchor 
+# 			)[0] 
 
-# Applying non-maximum suppression 
-boxes = config.nms(boxes, iou_threshold=1, threshold=0.7) 
+# # Applying non-maximum suppression 
+# boxes = config.nms(boxes, iou_threshold=1, threshold=0.7) 
 
-# Plotting the image with the bounding boxes 
-config.plot_image(x[0].permute(1,2,0).to("cpu"), boxes)
+# # Plotting the image with the bounding boxes 
+# config.plot_image(x[0].permute(1,2,0).to("cpu"), boxes)
 
 
 # Define the train function to train the model 
