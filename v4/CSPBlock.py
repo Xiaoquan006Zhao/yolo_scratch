abdd998775fb0f99cb14b1f17ed9a4c614033284
Xiaoquan006Zhao <xiaoquan0622@gmail.com>
2024-02-12 18:18:58 -0600

    CSPBlock finished

--- v4/CSPBlock.py
+++ v4/CSPBlock.py
@@ -3,9 +3,7 @@ import torch.nn as nn
 from IPython.display import Image
 import torchvision
 from torchview import draw_graph
-
 from denseBlock import *
-
 
 class CSPBlock(nn.Module):
     def __init__(self, process_block, in_channels):
@@ -37,20 +35,23 @@ class CSPBlock(nn.Module):
         out = torch.cat((part1, part2_dense), dim=1)
         return out
 
-test_shape = (1,3,224,224)
-def test_CSPBlock():
-    x = torch.randn(test_shape)
-    model = DenseBlock(5,3)
-    dense_block = DenseBlock(layer_num=4, in_channels=4)  # Example DenseBlock initialization
-    model = CSPBlock(process_block=dense_block, in_channels=8)
+# test_shape = (1,3,224,224)
+# x = torch.randn(test_shape)
+# conv = nn.Conv2d(3, 8, 3)
+# x = conv(x)
+    
+# def test_CSPBlock():
+#     model = DenseBlock(5,x.shape[1])
+#     dense_block = DenseBlock(layer_num=4, in_channels=4)  # Example DenseBlock initialization
+#     model = CSPBlock(process_block=dense_block, in_channels=8)
 
-    print('CSPblock Output shape : ',model(x).shape)
-    print('Model ',model)
-    # del model
-    return model
+#     print('CSPblock Output shape : ',model(x).shape)
+#     print('Model ',model)
+#     # del model
+#     return model
 
-model = test_CSPBlock()
+# model = test_CSPBlock()
 
-architecture = 'denseblock'
-model_graph = draw_graph(model, input_size=(test_shape), graph_dir ='TB' , roll=True, expand_nested=True, graph_name=f'self_{architecture}',save_graph=True,filename=f'self_{architecture}')
-model_graph.visual_graph
+# architecture = 'denseblock'
+# model_graph = draw_graph(model, input_size=(x.shape), graph_dir ='TB' , roll=True, expand_nested=True, graph_name=f'self_{architecture}',save_graph=True,filename=f'self_{architecture}')
+# model_graph.visual_graph
