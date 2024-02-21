79cf14307fc4a820c44ee0e8ec8beaf5d5932ec1
Xiaoquan006Zhao <xiaoquan0622@gmail.com>
2024-02-21 16:28:16 -0600

    test assert

--- v5/loss.py
+++ v5/loss.py
@@ -44,7 +44,7 @@ class YOLOLoss(nn.Module):
         class_loss = self.cross_entropy(pred[..., 5:][obj], target[..., 5][obj].long())
 
         loss = 2 * box_loss + object_loss + no_object_loss + class_loss 
-        assert not math.isnan(loss), f"{box_loss}, {object_loss}, {no_object_loss}, {class_loss}"
+        assert not math.isnan(loss), f"{box_loss}, {object_loss}, {no_object_loss}, {class_loss}, {cious}, \n {box_preds[obj]}, \n {target[..., 1:5][obj]}"
 
         return ( 
 			2 * box_loss 
