0a723e4c781700f63692a1810f490063d2ed62ab
Xiaoquan006Zhao <1009642031@qq.com>
2024-02-22 05:16:51 -0600

    2

--- v5/test.py
+++ v5/test.py
@@ -57,9 +57,9 @@ with torch.no_grad():
 	# Getting bounding boxes for each scale 
 	for i in range(3): 
 		batch_size, A, S, _, _ = output[i].shape 
-		anchor = config.scaled_anchors[i] 
+		anchors = config.scaled_anchors[i] 
 		anchors = anchors.reshape(1, len(anchors), 1, 1, 2) 
-		boxes_scale_i = decodePrediction(output[i], anchor, s=S) 
+		boxes_scale_i = decodePrediction(output[i], anchors, s=S) 
 		for idx, (box) in enumerate(boxes_scale_i): 
 			bboxes[idx] += box 
 model.train() 
