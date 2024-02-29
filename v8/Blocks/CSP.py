8cd9a98ce3871704dc94573b2eff2a8ac2689431
Xiaoquan006Zhao <xiaoquan0622@gmail.com>
2024-02-29 09:52:01 -0600

    concate list change

--- v8/Blocks/CSP.py
+++ v8/Blocks/CSP.py
@@ -14,7 +14,7 @@ class CSPBlock(nn.Module):
         super(CSPBlock, self).__init__()
 
         self.process_blocks = nn.ModuleList(
-            *[BottleNeck(in_channels//2, bottleNeck_use_residual) for _ in range(BottleNeck_repeats)],
+            [BottleNeck(out_channels//2, bottleNeck_use_residual) for _ in range(BottleNeck_repeats)],
         )
 
         self.BottleNeck_repeats = BottleNeck_repeats
@@ -24,8 +24,7 @@ class CSPBlock(nn.Module):
         self.in_channels = in_channels
 
         self.conv1 = ConvBNMish(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
-        self.conv2 = ConvBNMish(0.5*(BottleNeck_repeats+2)*out_channels, out_channels, kernel_size=1, stride=1, padding=0)
-
+        self.conv2 = ConvBNMish((BottleNeck_repeats+2)*out_channels//2, out_channels, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -33,12 +32,12 @@ class CSPBlock(nn.Module):
 
         part1, part2 = torch.split(x, x.shape[1] // 2, dim=1)  # Split channels into two halves
         concate_list.append(part1)
+        concate_list.append(part2)
 
         for process_block in self.process_blocks:
             part2 = process_block(part2)
             concate_list.append(part2)
 
-        concate_list.append(part2)
         out = torch.cat(concate_list, dim=1)
 
         if self.use_dropblock:
