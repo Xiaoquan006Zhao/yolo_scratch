e79d2e6071bac07feea72f02a51d804077aae779
Xiaoquan006Zhao <xiaoquan0622@gmail.com>
2024-02-29 09:29:20 -0600

    v8 initial push

---
+++ v8/train.py
@@ -0,0 +1,114 @@
+from config import Config
+import torch
+from dataset import Dataset
+from PIL import ImageFile 
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+import torch.optim as optim 
+from tqdm import tqdm
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+from model import YOLOv5
+from loss import YOLOLoss
+from utils import (
+	load_checkpoint,
+	save_checkpoint,
+)
+
+# Define the train function to train the model 
+def training_loop(e, loader, model, optimizer, scheduler, loss_fn, scaler, scaled_anchors, scales):
+	iters = len(loader) 
+	# Creating a progress bar 
+	progress_bar = tqdm(loader, leave=True) 
+
+	# Initializing a list to store the losses 
+	losses = [] 
+
+	# Iterating over the training data 
+	for i, (x, y) in enumerate(progress_bar): 
+		x = x.to(Config.device) 
+		y0, y1, y2 = ( 
+			y[0].to(Config.device), 
+			y[1].to(Config.device), 
+			y[2].to(Config.device), 
+		) 
+
+		with torch.cuda.amp.autocast(): 
+			# Getting the model predictions 
+			outputs = model(x) 
+
+			# Calculating the loss at each scale 
+			# the weight [4, 1, 0.4] is found in https://docs.ultralytics.com/yolov5/tutorials/architecture_description/#41-compute-losses
+			loss = ( 
+				loss_fn(outputs[0], y0, scaled_anchors[0], scales[0]) 
+				+ loss_fn(outputs[1], y1, scaled_anchors[1], scales[1]) 
+				+ loss_fn(outputs[2], y2, scaled_anchors[2], scales[2]) 
+			) 
+
+		# Add the loss to the list 
+		losses.append(loss.item()) 
+
+		# Reset gradients 
+		optimizer.zero_grad() 
+
+		# Backpropagate the loss 
+		scaler.scale(loss).backward() 
+
+		# Optimization step 
+		scaler.step(optimizer) 
+
+		scheduler.step(e + i / iters)
+
+		# Update the scaler for next iteration 
+		scaler.update() 
+
+		# update progress bar with loss 
+		mean_loss = sum(losses) / len(losses) 
+		progress_bar.set_postfix(loss=mean_loss)
+
+# Creating the model from YOLOv3 class 
+model = YOLOv5().to(Config.device) 
+
+# Defining the optimizer 
+optimizer = optim.Adam(model.parameters(), lr = Config.max_leanring_rate) 
+
+scheduler = CosineAnnealingWarmRestarts(optimizer, 
+										T_0 = 32,
+										T_mult = 1, # A factor increases TiTi​ after a restart
+										eta_min = Config.min_leanring_rate) 
+scheduler.base_lrs[0] = Config.max_leanring_rate
+
+# Defining the loss function 
+loss_fn = YOLOLoss() 
+
+# Defining the scaler for mixed precision training 
+scaler = torch.cuda.amp.GradScaler() 
+
+if Config.load_model: 
+	load_checkpoint(Config.checkpoint_file, model, optimizer, Config.max_leanring_rate) 
+
+# Defining the train dataset 
+train_dataset = Dataset( 
+	csv_file = Config.train_csv_file,
+	image_dir = Config.image_dir,
+	label_dir = Config.label_dir,
+	anchors=Config.ANCHORS, 
+	image_size = Config.image_size, 
+	grid_sizes = Config.s, 
+	transform=Config.train_transform 
+) 
+
+# Defining the train data loader 
+train_loader = torch.utils.data.DataLoader( 
+	train_dataset, 
+	batch_size = Config.batch_size, 
+	num_workers = 2, 
+	shuffle = True, 
+	pin_memory = True, 
+) 
+# Training the model 
+for e in range(1, Config.epochs+1): 
+	print("Epoch:", e) 
+	training_loop(e, train_loader, model, optimizer, scheduler, loss_fn, scaler, Config.scaled_anchors, Config.s) 
+
+	# Saving the model 
+	if Config.save_model: 
+		save_checkpoint(model, optimizer, filename=Config.checkpoint_file)
---
+++ v8/utils_metric.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+from config import Config
+from utils import (
+    ciou,
+    decodePrediction_bbox_no_offset,
+)
+
+def calculate_precision_recall(predictions, targets, scaled_anchor):
+    obj = targets[..., 0] == 1
+     # Reshaping anchors to match predictions
+    scaled_anchor = scaled_anchor.reshape(1, 3, 1, 1, 2)
+    
+    box_preds = decodePrediction_bbox_no_offset(predictions, scaled_anchor)
+    
+    cious = ciou(box_preds[obj], targets[..., 1:5][obj])
+
+     # Filter predictions based on CIoU threshold
+    true_positives = torch.sum(cious > Config.enough_overlap_threshold*1.1).item()
+    false_positives = torch.sum(cious <= Config.enough_overlap_threshold*1.1).item()
+    # targets[..., 1:5][obj].shape[0] is number of bounding boxes
+    false_negatives = targets[..., 1:5][obj].shape[0] - true_positives
+
+    precision = true_positives / (true_positives + false_positives + Config.numerical_stability)
+    recall = true_positives / (true_positives + false_negatives + Config.numerical_stability)
+
+    return precision, recall
