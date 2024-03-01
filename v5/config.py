aafc006023fe84c64183af445650c47f1121cc9c
Xiaoquan006Zhao <xiaoquan0622@gmail.com>
2024-03-01 17:57:01 -0600

    1

--- v5/config.py
+++ v5/config.py
@@ -47,10 +47,7 @@ class Config:
             self.numerical_stability = 1e-6
 
             self.image_size = 640
-            # Grid cell sizes 
             self.s = [self.image_size // 32, self.image_size // 16, self.image_size // 8] 
-            # Anchor boxes for each feature map scaled between 0 and 1 
-            # 3 feature maps at 3 different scales based on YOLOv3 paper 
 
             self.num_anchors = 3
 
@@ -58,12 +55,6 @@ class Config:
             print(f"-{self.device}-")
 
             self.ANCHORS = auto_anchor(self.num_anchors, self.label_dir, self.s)
-
-            # ANCHORS = [ 
-            # 	[(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)], 
-            # 	[(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)], 
-            # 	[(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)], 
-            # ] 
 
             self.scaled_anchors = ( 
                 torch.tensor(self.ANCHORS) *
@@ -83,67 +74,34 @@ class Config:
 
             self.train_transform = A.Compose( 
                 [ 
-                    # just resize without respecting the aspect ratio and padding
                     A.Resize(height=self.image_size, width=self.image_size),
-                    # A.LongestMaxSize(max_size=image_size),
-                    # A.PadIfNeeded(
-                    #     min_height=image_size, min_width=image_size, border_mode=cv2.BORDER_CONSTANT
-                    # ),
                     A.ColorJitter(
                         brightness=0.5, contrast=0.5,
                         saturation=0.5, hue=0.5, p=0.5
                     ),
-                    # A.RandomCrop(width=256, height=256, p=0.5),
                     A.Rotate(limit=45, p=0.5),
                     A.HorizontalFlip(p=0.5),
-
-                    # Additional transformations
-                    #A.VerticalFlip(p=0.5),  # Flip the image vertically
-                    # Adjust brightness and contrast
                     A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),  
-                    # Add Gaussian noise
-                    #A.GaussNoise(var_limit=(10.0, 50.0), p=0.5),  
-                    # Affine transformations: shift, scale, rotate
-                    #A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=45, p=0.5),  
-                    # Elastic deformation
-                    #A.ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=0.5),  
-                    
-                    # Normalize the image 
                     A.Normalize( 
                         mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255
                     ), 
-                    # Convert the image to PyTorch tensor 
                     ToTensorV2() 
                 ], 
-                # Augmentation for bounding boxes 
-                bbox_params=A.BboxParams( 
-                                format="yolo", 
-                                min_visibility=0.4, 
-                                label_fields=[] 
-                            ) 
+                bbox_params=A.BboxParams( format="yolo", min_visibility=0.4, label_fields=[]) 
             ) 
 
             self.test_transform = A.Compose( 
                 [ 
-                    # Rescale an image so that maximum side is equal to image_size 
                     A.LongestMaxSize(max_size=self.image_size), 
-                    # Pad remaining areas with zeros 
                     A.PadIfNeeded( 
                         min_height=self.image_size, min_width=self.image_size, border_mode=cv2.BORDER_CONSTANT 
                     ), 
-                    # Normalize the image 
                     A.Normalize( 
                         mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255
                     ), 
-                    # Convert the image to PyTorch tensor 
                     ToTensorV2() 
                 ], 
-                # Augmentation for bounding boxes 
-                bbox_params=A.BboxParams( 
-                                format="yolo", 
-                                min_visibility=0.4, 
-                                label_fields=[] 
-                            ) 
+                bbox_params=A.BboxParams( format="yolo", min_visibility=0.4, label_fields=[]) 
             )
 
 Config.initialize()
