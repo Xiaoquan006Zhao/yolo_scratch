4c4fffca86de53ed852ab10c1b887be00354c099
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-01 12:52:57 -0600

    1

--- v8/utils_metric.py
+++ v8/utils_metric.py
@@ -9,21 +9,25 @@ from utils import (
 
 def calculate_precision_recall(predictions, targets, scaled_anchor):
     obj = targets[..., 0] == 1
+    obj_cpu = obj.cpu().numpy()
+    num_obj = np.count_nonzero(obj_cpu)
+
     pred_obj = predictions[..., 0] == 1
-    
-     # Reshaping anchors to match predictions
+    pred_obj_cpu = pred_obj.cpu().numpy()
+    num_pred_obj = np.count_nonzero(pred_obj_cpu)
+
+    # Reshaping anchors to match predictions
     scaled_anchor = scaled_anchor.reshape(1, 3, 1, 1, 2)
     
     box_preds = decodePrediction_bbox_no_offset(predictions, scaled_anchor)
-    
     cious = ciou(box_preds[obj], targets[..., 1:5][obj])
 
      # Filter predictions based on CIoU threshold
     true_positives = torch.sum(cious > Config.enough_overlap_threshold*1.1).item()
-    false_positives = np.count_nonzero(pred_obj) - true_positives
+    false_positives = num_pred_obj - true_positives
 
     # targets[..., 1:5][obj].shape[0] is number of bounding boxes
-    false_negatives = np.count_nonzero(obj) - true_positives
+    false_negatives = num_obj - true_positives
 
     precision = true_positives / (true_positives + false_positives + Config.numerical_stability)
     recall = true_positives / (true_positives + false_negatives + Config.numerical_stability)
