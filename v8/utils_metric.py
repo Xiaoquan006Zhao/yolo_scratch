353608d8002edf6d09a507640a057c1b2a4760e9
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-01 12:25:24 -0600

    1

--- v8/utils_metric.py
+++ v8/utils_metric.py
@@ -8,7 +8,8 @@ from utils import (
 
 def calculate_precision_recall(predictions, targets, scaled_anchor):
     obj = targets[..., 0] == 1
-    pred_obj = predictions[..., 0] == 1
+    num_predictions = len(predictions[..., 0] == 1)
+    
      # Reshaping anchors to match predictions
     scaled_anchor = scaled_anchor.reshape(1, 3, 1, 1, 2)
     
@@ -18,7 +19,8 @@ def calculate_precision_recall(predictions, targets, scaled_anchor):
 
      # Filter predictions based on CIoU threshold
     true_positives = torch.sum(cious > Config.enough_overlap_threshold*1.1).item()
-    false_positives = len(predictions[pred_obj]) - true_positives
+    false_positives = num_predictions - true_positives
+
     # targets[..., 1:5][obj].shape[0] is number of bounding boxes
     false_negatives = targets[..., 1:5][obj].shape[0] - true_positives
 
