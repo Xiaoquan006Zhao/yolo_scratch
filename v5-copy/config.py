b307e27d0d5841ebf73ba4dc35310146fc0d2bea
Xiaoquan006Zhao <xiaoquan0622@gmail.com>
2024-03-01 18:08:51 -0600

    what's wrong

--- v5-copy/config.py
+++ v5-copy/config.py
@@ -27,10 +27,10 @@ class Config:
                 self.label_dir = os.path.join(base_dir, "data", dataset, "labels")
                 self.checkpoint_file = os.path.join(base_dir, "v5", f"{dataset}_checkpoint.pth.tar")
             else:
-                #self.train_csv_file = f"../data/{dataset}/100examples.csv"
-                #self.test_csv_file = f"../data/{dataset}/100examples_test.csv"
-                self.train_csv_file = f"../data/{dataset}/train.csv"
-                self.test_csv_file = f"../data/{dataset}/test.csv"
+                self.train_csv_file = f"../data/{dataset}/100examples.csv"
+                self.test_csv_file = f"../data/{dataset}/100examples_test.csv"
+                #self.train_csv_file = f"../data/{dataset}/train.csv"
+                #self.test_csv_file = f"../data/{dataset}/test.csv"
                 self.image_dir = f"../data/{dataset}/images/"
                 self.label_dir = f"../data/{dataset}/labels/"  
                 self.checkpoint_file = f"{dataset}_checkpoint.pth.tar"
@@ -47,10 +47,7 @@ class Config:
             self.numerical_stability = 1e-6
 
             self.image_size = 640
-            # Grid cell sizes 
             self.s = [self.image_size // 32, self.image_size // 16, self.image_size // 8] 
-            # Anchor boxes for each feature map scaled between 0 and 1 
-            # 3 feature maps at 3 different scales based on YOLOv3 paper 
 
             self.num_anchors = 3
 
@@ -58,12 +55,6 @@ class Config:
             print(f"-{self.device}-")
 
             self.ANCHORS = auto_anchor(self.num_anchors, self.label_dir, self.s)
-
-            # ANCHORS = [ 
-            # 	[(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)], 
-            # 	[(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)], 
-            # 	[(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)], 
-            # ] 
 
             self.scaled_anchors = ( 
                 torch.tensor(self.ANCHORS) *
@@ -81,71 +72,36 @@ class Config:
             self.valid_prediction_threshold = 0.6
             self.enough_overlap_threshold = 0.6
 
-            # Transform for training 
             self.train_transform = A.Compose( 
                 [ 
-                    # just resize without respecting the aspect ratio and padding
                     A.Resize(height=self.image_size, width=self.image_size),
-                    # A.LongestMaxSize(max_size=image_size),
-                    # A.PadIfNeeded(
-                    #     min_height=image_size, min_width=image_size, border_mode=cv2.BORDER_CONSTANT
-                    # ),
                     A.ColorJitter(
                         brightness=0.5, contrast=0.5,
                         saturation=0.5, hue=0.5, p=0.5
                     ),
-                    # A.RandomCrop(width=256, height=256, p=0.5),
                     A.Rotate(limit=45, p=0.5),
                     A.HorizontalFlip(p=0.5),
-
-                    # Additional transformations
-                    #A.VerticalFlip(p=0.5),  # Flip the image vertically
-                    # Adjust brightness and contrast
                     A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),  
-                    # Add Gaussian noise
-                    #A.GaussNoise(var_limit=(10.0, 50.0), p=0.5),  
-                    # Affine transformations: shift, scale, rotate
-                    #A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=45, p=0.5),  
-                    # Elastic deformation
-                    #A.ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=0.5),  
-                    
-                    # Normalize the image 
                     A.Normalize( 
                         mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255
                     ), 
-                    # Convert the image to PyTorch tensor 
                     ToTensorV2() 
                 ], 
-                # Augmentation for bounding boxes 
-                bbox_params=A.BboxParams( 
-                                format="yolo", 
-                                min_visibility=0.4, 
-                                label_fields=[] 
-                            ) 
+                bbox_params=A.BboxParams( format="yolo", min_visibility=0.4, label_fields=[]) 
             ) 
 
-            # Transform for testing 
             self.test_transform = A.Compose( 
                 [ 
-                    # Rescale an image so that maximum side is equal to image_size 
                     A.LongestMaxSize(max_size=self.image_size), 
-                    # Pad remaining areas with zeros 
                     A.PadIfNeeded( 
                         min_height=self.image_size, min_width=self.image_size, border_mode=cv2.BORDER_CONSTANT 
                     ), 
-                    # Normalize the image 
                     A.Normalize( 
                         mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255
                     ), 
-                    # Convert the image to PyTorch tensor 
                     ToTensorV2() 
                 ], 
-                # Augmentation for bounding boxes 
-                bbox_params=A.BboxParams( 
-                                format="yolo", 
-                                min_visibility=0.4, 
-                                label_fields=[] 
-                            ) 
+                bbox_params=A.BboxParams( format="yolo", min_visibility=0.4, label_fields=[]) 
             )
 
 Config.initialize()
--- v5-copy/train.py
+++ v5-copy/train.py
@@ -13,16 +13,12 @@ from utils import (
 	save_checkpoint,
 )
 
-# Define the train function to train the model 
 def training_loop(e, loader, model, optimizer, scheduler, loss_fn, scaler, scaled_anchors, scales):
 	iters = len(loader) 
-	# Creating a progress bar 
 	progress_bar = tqdm(loader, leave=True) 
 
-	# Initializing a list to store the losses 
 	losses = [] 
 
-	# Iterating over the training data 
 	for i, (x, y) in enumerate(progress_bar): 
 		x = x.to(Config.device) 
 		y0, y1, y2 = ( 
@@ -32,42 +28,31 @@ def training_loop(e, loader, model, optimizer, scheduler, loss_fn, scaler, scale
 		) 
 
 		with torch.cuda.amp.autocast(): 
-			# Getting the model predictions 
 			outputs = model(x) 
 
-			# Calculating the loss at each scale 
-			# the weight [4, 1, 0.4] is found in https://docs.ultralytics.com/yolov5/tutorials/architecture_description/#41-compute-losses
 			loss = ( 
 				loss_fn(outputs[0], y0, scaled_anchors[0], scales[0]) 
 				+ loss_fn(outputs[1], y1, scaled_anchors[1], scales[1]) 
 				+ loss_fn(outputs[2], y2, scaled_anchors[2], scales[2]) 
 			) 
 
-		# Add the loss to the list 
 		losses.append(loss.item()) 
 
-		# Reset gradients 
 		optimizer.zero_grad() 
 
-		# Backpropagate the loss 
 		scaler.scale(loss).backward() 
 
-		# Optimization step 
 		scaler.step(optimizer) 
 
 		scheduler.step(e + i / iters)
 
-		# Update the scaler for next iteration 
 		scaler.update() 
 
-		# update progress bar with loss 
 		mean_loss = sum(losses) / len(losses) 
 		progress_bar.set_postfix(loss=mean_loss)
 
-# Creating the model from YOLOv3 class 
 model = YOLOv5().to(Config.device) 
 
-# Defining the optimizer 
 optimizer = optim.Adam(model.parameters(), lr = Config.max_leanring_rate) 
 
 scheduler = CosineAnnealingWarmRestarts(optimizer, 
@@ -76,16 +61,13 @@ scheduler = CosineAnnealingWarmRestarts(optimizer,
 										eta_min = Config.min_leanring_rate) 
 scheduler.base_lrs[0] = Config.max_leanring_rate
 
-# Defining the loss function 
 loss_fn = YOLOLoss() 
 
-# Defining the scaler for mixed precision training 
 scaler = torch.cuda.amp.GradScaler() 
 
 if Config.load_model: 
 	load_checkpoint(Config.checkpoint_file, model, optimizer, Config.max_leanring_rate) 
 
-# Defining the train dataset 
 train_dataset = Dataset( 
 	csv_file = Config.train_csv_file,
 	image_dir = Config.image_dir,
@@ -96,7 +78,6 @@ train_dataset = Dataset(
 	transform=Config.train_transform 
 ) 
 
-# Defining the train data loader 
 train_loader = torch.utils.data.DataLoader( 
 	train_dataset, 
 	batch_size = Config.batch_size, 
@@ -104,11 +85,10 @@ train_loader = torch.utils.data.DataLoader(
 	shuffle = True, 
 	pin_memory = True, 
 ) 
-# Training the model 
+
 for e in range(1, Config.epochs+1): 
 	print("Epoch:", e) 
 	training_loop(e, train_loader, model, optimizer, scheduler, loss_fn, scaler, Config.scaled_anchors, Config.s) 
 
-	# Saving the model 
 	if Config.save_model: 
-		save_checkpoint(model, optimizer, filename=Config.checkpoint_file)
+		save_checkpoint(model, optimizer, Config.checkpoint_file)
