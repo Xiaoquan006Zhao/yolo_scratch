ca7e034711badfa6bd95c5f4ba12d9303d1bf04e
Xiaoquan006Zhao <xiaoquan0622@gmail.com>
2024-03-01 20:35:48 -0600

    2

--- v5/utils_metric.py
+++ v5/utils_metric.py
@@ -12,13 +12,16 @@ def calculate_precision_recall(predictions, targets, scaled_anchor, s):
     decoded_predictions = decodePrediction(predictions, scaled_anchor, s)
     decoded_targets = targets.reshape(len(decoded_predictions), len(decoded_predictions[0]), 6)
 
+    num_predictions = len([1 for prediction_batch in decoded_predictions for prediction in prediction_batch if prediction[0] > Config.valid_prediction_threshold])
+    num_targets = len([1 for target_batch in decoded_targets for target in target_batch if target[0] > Config.valid_prediction_threshold])
+
     # same class and ground truth labeled as positive objectiveness
     potential_TP = [(pred[5] == target[5]) and (target[0] > Config.valid_prediction_threshold) for pred, target in zip(decoded_predictions, decoded_targets)]
 
-    num_predictions = len([1 for prediction in decoded_predictions if prediction[0] > Config.valid_prediction_threshold])
-    num_targets = len([1 for target in decoded_targets if target[0] > Config.valid_prediction_threshold])
+    ious = ciou([pred[1:5] for pred, is_tp in zip(decoded_predictions, potential_TP) if is_tp],
+                [target[1:5] for target, is_tp in zip(decoded_targets, potential_TP) if is_tp],
+                is_pred=False)
 
-    ious = ciou(np.array(decoded_predictions)[potential_TP, 1:5], np.array(decoded_targets)[potential_TP, 1:5], is_pred=False)
 
     true_positives = torch.sum(ious > Config.enough_overlap_threshold).item()
     false_positives = num_predictions - true_positives
