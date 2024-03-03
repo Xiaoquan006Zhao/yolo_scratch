4c3565e2182c6e58f1b9de79b5e91e4f2f8344fe
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-02 22:44:56 -0600

    1

--- v8/utils_metric.py
+++ v8/utils_metric.py
@@ -12,18 +12,34 @@ def calculate_precision_recall(predictions, targets, scaled_anchor, s):
     decoded_predictions = decodePrediction(predictions, scaled_anchor, s, to_list=False)
     decoded_targets = targets.reshape(decoded_predictions.shape)
 
-    decoded_predictions_flat = decoded_predictions.view(-1, decoded_predictions.shape[-1])
-    decoded_targets_flat = decoded_targets.view(-1, decoded_targets.shape[-1])
+    # num_predictions = 0
+    # num_targets = 0
+    # true_positives = 0
 
-    decoded_predictions_nms_flat = torch.tensor(nms(decoded_predictions_flat.tolist())).view(decoded_predictions.shape[0], -1, 6)
+    # for batch_idx in range(Config.test_batch_size):
+    #     decoded_predictions_batch = decoded_predictions[batch_idx]
+    #     decoded_targets_batch = decoded_targets[batch_idx]
 
-    num_predictions = torch.sum(decoded_predictions_nms_flat[:, :, 0] > Config.valid_prediction_threshold).item()
-    num_targets = torch.sum(decoded_targets_flat[:, 0] > Config.valid_prediction_threshold).item()
+    #     decoded_predictions_nms = torch.tensor(nms(decoded_predictions_batch.tolist())).view(1, -1, 6)
+    #     num_predictions += torch.sum(decoded_predictions_nms[:, :, 0] > Config.valid_prediction_threshold).item()
+    #     num_targets += torch.sum(decoded_targets_batch[:, 0] > Config.valid_prediction_threshold).item()
 
-    tar_obj = decoded_targets_flat[:, 0] > Config.valid_prediction_threshold
-    pred_obj = decoded_predictions_flat[:, 0] > Config.valid_prediction_threshold
+    #     tar_obj = decoded_targets_batch[..., 0] > Config.valid_prediction_threshold
+    #     pred_obj = decoded_predictions_batch[..., 0] > Config.valid_prediction_threshold
 
-    ious = ciou(decoded_predictions_flat[:, 1:5][pred_obj & tar_obj], decoded_targets_flat[:, 1:5][pred_obj & tar_obj], is_pred=False)
+    #     ious = ciou(decoded_predictions_batch[..., 1:5][pred_obj & tar_obj], decoded_targets_batch[..., 1:5][pred_obj & tar_obj], is_pred=False)
+
+    #     true_positives += torch.sum(ious > Config.enough_overlap_threshold).item()
+
+
+    decoded_predictions_nms = torch.tensor(nms(decoded_predictions)).view(1, -1, 6)
+    num_predictions = torch.sum(decoded_predictions_nms[:, :, 0] > Config.valid_prediction_threshold).item()
+    num_targets = torch.sum(decoded_targets[:, :, 0] > Config.valid_prediction_threshold).item()
+
+    tar_obj = decoded_targets[..., 0] > Config.valid_prediction_threshold
+    pred_obj = decoded_predictions[..., 0] > Config.valid_prediction_threshold
+
+    ious = ciou(decoded_predictions[..., 1:5][pred_obj & tar_obj], decoded_targets[..., 1:5][pred_obj & tar_obj], is_pred=False)
 
     true_positives = torch.sum(ious > Config.enough_overlap_threshold).item()
 
