d5759bd59ab75725ceb1af763c20d243f5e5aed4
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-03 23:20:25 -0600

    loss

--- v5/loss.py
+++ v5/loss.py
@@ -12,7 +12,11 @@ class YOLOLoss(nn.Module):
         self.bce = nn.BCEWithLogitsLoss()
         self.cross_entropy = nn.CrossEntropyLoss(label_smoothing=0.1)
         self.sigmoid = nn.Sigmoid()
-        self.norm = nn.BatchNorm1d(1)
+        
+        self.weight_box = nn.Parameter(torch.ones(1))
+        self.weight_object = nn.Parameter(torch.ones(1))
+        self.weight_no_object = nn.Parameter(torch.ones(1))
+        self.weight_class = nn.Parameter(torch.ones(1))
 
     def forward(self, pred, target, scaled_anchor, scale):
         obj = target[..., 0] == 1
@@ -29,7 +33,12 @@ class YOLOLoss(nn.Module):
         object_loss = self.bce(self.sigmoid(pred[..., 0:1][obj]), target[..., 0:1][obj])
         class_loss = self.cross_entropy(pred[..., 5:][obj], target[..., 5][obj].long())
 
-        loss = self.norm(box_loss) + self.norm(object_loss) + self.norm(no_object_loss) + self.norm(class_loss)
+        loss = (
+            self.weight_box * box_loss +
+            self.weight_object * object_loss +
+            self.weight_no_object * no_object_loss +
+            self.weight_class * class_loss
+        )
         # assert not math.isnan(loss), f"{box_loss}, {object_loss}, {no_object_loss}, {class_loss}, {cious}, \n {box_preds[obj]}, \n {target[..., 1:5][obj]}"
 
-        return (loss)
+        return loss
