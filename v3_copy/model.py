57c74c3e9958018acb190cceda41b66f02bf7af2
Xiaoquan006Zhao <xiaoquan0622@gmail.com>
2024-03-04 16:17:10 -0600

    2

--- v3_copy/model.py
+++ v3_copy/model.py
@@ -85,18 +85,22 @@ class YOLOv3(nn.Module):
 		self.in_channels = in_channels 
 
 		self.layers = nn.ModuleList([ 
-			ConvBNMish(in_channels, 64, kernel_size=6, stride=2, padding=2), 
+			ConvBNMish(in_channels, 32, kernel_size=3, stride=1, padding=1), 
+			ConvBNMish(32, 64, kernel_size=3, stride=2, padding=1), 
+			CSPBlock(64, 64, bottleNeck_use_residual=True, BottleNeck_repeats=1),
+
 			ConvBNMish(64, 128, kernel_size=3, stride=2, padding=1), 
-			CSPBlock(128, 128, bottleNeck_use_residual=True, BottleNeck_repeats=3),
+			CSPBlock(128, 128, bottleNeck_use_residual=True, BottleNeck_repeats=2),
 
 			ConvBNMish(128, 256, kernel_size=3, stride=2, padding=1), 
-			CSPBlock(256, 256, bottleNeck_use_residual=True, BottleNeck_repeats=6),
+			CSPBlock(256, 256, bottleNeck_use_residual=True, BottleNeck_repeats=8),
 
 			ConvBNMish(256, 512, kernel_size=3, stride=2, padding=1), 
-			CSPBlock(512, 512, bottleNeck_use_residual=True, BottleNeck_repeats=9),
+			ResidualBlock(512, num_repeats=8), 
+			CSPBlock(512, 512, bottleNeck_use_residual=True, BottleNeck_repeats=8),
 
 			ConvBNMish(512, 1024, kernel_size=3, stride=2, padding=1), 
-			CSPBlock(1024, 1024, bottleNeck_use_residual=True, BottleNeck_repeats=3),
+			CSPBlock(1024, 1024, bottleNeck_use_residual=True, BottleNeck_repeats=4),
 
 			SPPFBlock(1024, pool_size=5, pool_repeats=3),
 			PAN(config.PAN_channels, num_classes=config.num_classes),
@@ -110,8 +114,8 @@ class YOLOv3(nn.Module):
 				return layer(route_connections)
 			
 			x = layer(x) 
-
-			if isinstance(layer, CSPBlock) and (layer.BottleNeck_repeats == 6 or layer.BottleNeck_repeats == 9): 
+			
+			if isinstance(layer, ResidualBlock) and (layer.num_repeats == 8): 
 				route_connections.append(x) 
 
 			if isinstance(layer, SPPFBlock): 
