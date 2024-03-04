902a464b73325c420808fd261364d34950f39e23
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-04 12:49:10 -0600

    csp incestigation

--- v3 copy/model.py
+++ v3 copy/model.py
@@ -7,7 +7,8 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import matplotlib.patches as patches 
 from Blocks.BasicBlock import ConvBNMish
-  
+from Blocks.CSP import CSPBlock
+
 
 class CNNBlock(nn.Module): 
 	def __init__(self, in_channels, out_channels, use_batch_norm=True, **kwargs): 
@@ -82,15 +83,25 @@ class YOLOv3(nn.Module):
 		self.layers = nn.ModuleList([ 
 			CNNBlock(in_channels, 32, kernel_size=3, stride=1, padding=1), 
 			CNNBlock(32, 64, kernel_size=3, stride=2, padding=1), 
-			ResidualBlock(64, num_repeats=1), 
+			# ResidualBlock(64, num_repeats=1), 
+			CSPBlock(64, 64, bottleNeck_use_residual=True, BottleNeck_repeats=1),
+
 			CNNBlock(64, 128, kernel_size=3, stride=2, padding=1), 
 			ResidualBlock(128, num_repeats=2), 
+			CSPBlock(128, 128, bottleNeck_use_residual=True, BottleNeck_repeats=2),
+
 			CNNBlock(128, 256, kernel_size=3, stride=2, padding=1), 
 			ResidualBlock(256, num_repeats=8), 
+			CSPBlock(256, 256, bottleNeck_use_residual=True, BottleNeck_repeats=8),
+
 			CNNBlock(256, 512, kernel_size=3, stride=2, padding=1), 
 			ResidualBlock(512, num_repeats=8), 
+			CSPBlock(512, 512, bottleNeck_use_residual=True, BottleNeck_repeats=8),
+
 			CNNBlock(512, 1024, kernel_size=3, stride=2, padding=1), 
 			ResidualBlock(1024, num_repeats=4), 
+			CSPBlock(1024, 1024, bottleNeck_use_residual=True, BottleNeck_repeats=4),
+
 			CNNBlock(1024, 512, kernel_size=1, stride=1, padding=0), 
 			CNNBlock(512, 1024, kernel_size=3, stride=1, padding=1), 
 			ResidualBlock(1024, use_residual=False, num_repeats=1), 
