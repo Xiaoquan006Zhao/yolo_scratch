3c186703d332addf537bb75e1cde20434070eebf
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-03 23:52:44 -0600

    1

--- v5/train.py
+++ v5/train.py
@@ -53,7 +53,7 @@ loss_fn = YOLOLoss()
 optimizer = optim.Adam(
     [
         {'params': model.parameters()},
-        {'params': loss_fn.parameters()}, 
+        {'params': loss_fn.parameters(), 'lr': Config.loss_learning_rate}, 
     ],
     lr=Config.max_leanring_rate
 )
