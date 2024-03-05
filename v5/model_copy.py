e4a7fdc89aa34e1d5990681ad9af8c8193a77e02
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-04 19:51:58 -0600

    7

--- v5/model_copy.py
+++ v5/model_copy.py
@@ -3,10 +3,16 @@ import torch.nn as nn
   
 import numpy as np 
 import pandas as pd 
-  
+import config
 import matplotlib.pyplot as plt 
 import matplotlib.patches as patches 
-  
+from Blocks.BasicBlock import ConvBNMish
+from Blocks.CSP import CSPBlock
+
+from Blocks.PAN import PAN
+from Blocks.SPPF import SPPFBlock
+
+
 
 class CNNBlock(nn.Module): 
 	def __init__(self, in_channels, out_channels, use_batch_norm=True, **kwargs): 
@@ -32,12 +38,14 @@ class ResidualBlock(nn.Module):
 		for _ in range(num_repeats): 
 			res_layers += [ 
 				nn.Sequential( 
-					nn.Conv2d(channels, channels // 2, kernel_size=1), 
-					nn.BatchNorm2d(channels // 2), 
-					nn.LeakyReLU(0.1), 
-					nn.Conv2d(channels // 2, channels, kernel_size=3, padding=1), 
-					nn.BatchNorm2d(channels), 
-					nn.LeakyReLU(0.1) 
+					ConvBNMish(channels, out_channels=channels, kernel_size=1, stride=1, padding=0),
+        			ConvBNMish(channels, out_channels=channels, kernel_size=3, stride=1, padding=1),
+					# nn.Conv2d(channels, channels // 2, kernel_size=1), 
+					# nn.BatchNorm2d(channels // 2), 
+					# nn.LeakyReLU(0.1), 
+					# nn.Conv2d(channels // 2, channels, kernel_size=3, padding=1), 
+					# nn.BatchNorm2d(channels), 
+					# nn.LeakyReLU(0.1) 
 				) 
 			] 
 		self.layers = nn.ModuleList(res_layers) 
@@ -70,6 +78,49 @@ class ScalePrediction(nn.Module):
 		output = output.permute(0, 1, 3, 4, 2) 
 		return output
 
+# class YOLOv3(nn.Module): 
+# 	def __init__(self, in_channels=3, num_classes=20): 
+# 		super().__init__() 
+# 		self.num_classes = num_classes 
+# 		self.in_channels = in_channels 
+
+# 		self.layers = nn.ModuleList([ 
+# 			ConvBNMish(in_channels, 64, kernel_size=6, stride=2, padding=2), 
+# 			ConvBNMish(64, 128, kernel_size=3, stride=2, padding=1), 
+# 			CSPBlock(128, 128, bottleNeck_use_residual=True, BottleNeck_repeats=3),
+
+# 			ConvBNMish(128, 256, kernel_size=3, stride=2, padding=1), 
+# 			CSPBlock(256, 256, bottleNeck_use_residual=True, BottleNeck_repeats=6),
+
+# 			ConvBNMish(256, 512, kernel_size=3, stride=2, padding=1), 
+# 			CSPBlock(512, 512, bottleNeck_use_residual=True, BottleNeck_repeats=9),
+
+# 			ConvBNMish(512, 1024, kernel_size=3, stride=2, padding=1), 
+# 			CSPBlock(1024, 1024, bottleNeck_use_residual=True, BottleNeck_repeats=3),
+
+			
+
+# 			SPPFBlock(1024, pool_size=5, pool_repeats=3),
+# 			PAN(config.PAN_channels, num_classes=config.num_classes),
+# 		]) 
+	
+# 	def forward(self, x): 
+# 		route_connections = [] 
+
+# 		for layer in self.layers: 
+# 			if isinstance(layer, PAN):
+# 				return layer(route_connections)
+			
+# 			x = layer(x) 
+			
+# 			if isinstance(layer, CSPBlock) and (layer.BottleNeck_repeats == 6 or layer.BottleNeck_repeats == 9): 
+# 				route_connections.append(x) 
+
+# 			if isinstance(layer, SPPFBlock): 
+# 				route_connections.append(x) 
+				
+# 		# return outputs
+
 class YOLOv3(nn.Module): 
 	def __init__(self, in_channels=3, num_classes=20): 
 		super().__init__() 
@@ -77,6 +128,19 @@ class YOLOv3(nn.Module):
 		self.in_channels = in_channels 
 
 		self.layers = nn.ModuleList([ 
+			# ConvBNMish(in_channels, 64, kernel_size=6, stride=2, padding=2), 
+			# ConvBNMish(64, 128, kernel_size=3, stride=2, padding=1), 
+			# CSPBlock(128, 128, bottleNeck_use_residual=True, BottleNeck_repeats=3),
+
+			# ConvBNMish(128, 256, kernel_size=3, stride=2, padding=1), 
+			# CSPBlock(256, 256, bottleNeck_use_residual=True, BottleNeck_repeats=6),
+
+			# ConvBNMish(256, 512, kernel_size=3, stride=2, padding=1), 
+			# CSPBlock(512, 512, bottleNeck_use_residual=True, BottleNeck_repeats=9),
+
+			# ConvBNMish(512, 1024, kernel_size=3, stride=2, padding=1), 
+			# CSPBlock(1024, 1024, bottleNeck_use_residual=True, BottleNeck_repeats=3),
+
 			CNNBlock(in_channels, 32, kernel_size=3, stride=1, padding=1), 
 			CNNBlock(32, 64, kernel_size=3, stride=2, padding=1), 
 			ResidualBlock(64, num_repeats=1), 
@@ -121,6 +185,7 @@ class YOLOv3(nn.Module):
 			
 			x = layer(x) 
 
+			# if isinstance(layer, CSPBlock) and (layer.BottleNeck_repeats == 6 or layer.BottleNeck_repeats == 9): 
 			if isinstance(layer, ResidualBlock) and layer.num_repeats == 8: 
 				route_connections.append(x) 
 			
@@ -128,5 +193,6 @@ class YOLOv3(nn.Module):
 				x = torch.cat([x, route_connections[-1]], dim=1) 
 				route_connections.pop() 
 		return outputs
+
 
 
