fe25329519a88e822a9451b2937ea585bff5fd6e
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-04 23:58:58 -0600

    1

--- v5/utils.py
+++ v5/utils.py
@@ -29,7 +29,7 @@ def ciou(box1, box2, IoU_mode=False, WidthHeight_model=False):
 		v = stable_divide(4, np.pi ** 2) * ((torch.atan(box1[..., 2] / box1[..., 3]) - torch.atan(box2[..., 2] / box2[..., 3])) ** 2)
 		alpha = stable_divide(v, 1 - iou_score + v)
 
-		ciou_score = iou_score - stable_divide(center_distance, c_diag).unsqueeze(1) - (alpha * v)..unsqueeze(1)
+		ciou_score = iou_score - stable_divide(center_distance, c_diag).unsqueeze(1) - (alpha * v).unsqueeze(1)
 
 		return ciou_score
 	else: 
