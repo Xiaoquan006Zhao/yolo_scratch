8cd617b9db8c4caaa1619bbd618bdd09fff7dc9e
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-04 23:39:38 -0600

    1

--- v5/utils.py
+++ v5/utils.py
@@ -7,9 +7,17 @@ import os
 
 def ciou(box1, box2, IoU_mode=False, WidthHeight_model=False): 
 	if not WidthHeight_model: 
-		b1_x1, b1_y1, b1_x2, b1_y2 = box1[..., 0] - box1[..., 2] / 2, box1[..., 1] - box1[..., 3] / 2, box1[..., 0] + box1[..., 2] / 2, box1[..., 1] + box1[..., 3] / 2
-		b2_x1, b2_y1, b2_x2, b2_y2 = box2[..., 0] - box2[..., 2] / 2, box2[..., 1] - box2[..., 3] / 2, box2[..., 0] + box2[..., 2] / 2, box2[..., 1] + box2[..., 3] / 2
-		
+		# b1_x1, b1_y1, b1_x2, b1_y2 = box1[..., 0] - box1[..., 2] / 2, box1[..., 1] - box1[..., 3] / 2, box1[..., 0] + box1[..., 2] / 2, box1[..., 1] + box1[..., 3] / 2
+		# b2_x1, b2_y1, b2_x2, b2_y2 = box2[..., 0] - box2[..., 2] / 2, box2[..., 1] - box2[..., 3] / 2, box2[..., 0] + box2[..., 2] / 2, box2[..., 1] + box2[..., 3] / 2
+		b1_x1 = box1[..., 0:1] - box1[..., 2:3] / 2
+		b1_y1 = box1[..., 1:2] - box1[..., 3:4] / 2
+		b1_x2 = box1[..., 0:1] + box1[..., 2:3] / 2
+		b1_y2 = box1[..., 1:2] + box1[..., 3:4] / 2
+
+		b2_x1 = box2[..., 0:1] - box2[..., 2:3] / 2
+		b2_y1 = box2[..., 1:2] - box2[..., 3:4] / 2
+		b2_x2 = box2[..., 0:1] + box2[..., 2:3] / 2
+		b2_y2 = box2[..., 1:2] + box2[..., 3:4] / 2
 		x1 = torch.max(b1_x1, b2_x1) 
 		y1 = torch.max(b1_y1, b2_y1) 
 		x2 = torch.min(b1_x2, b2_x2) 
