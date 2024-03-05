68745458861eb111603d1010afbc883acb4632bb
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-05 17:31:26 -0600

    1

--- v5/utils.py
+++ v5/utils.py
@@ -5,171 +5,138 @@ import matplotlib.patches as patches
 import config
 import os
 
-def iou(box1, box2, is_pred=True): 
-	if is_pred: 
-		b1_x1 = box1[..., 0:1] - box1[..., 2:3] / 2
-		b1_y1 = box1[..., 1:2] - box1[..., 3:4] / 2
-		b1_x2 = box1[..., 0:1] + box1[..., 2:3] / 2
-		b1_y2 = box1[..., 1:2] + box1[..., 3:4] / 2
-
-		b2_x1 = box2[..., 0:1] - box2[..., 2:3] / 2
-		b2_y1 = box2[..., 1:2] - box2[..., 3:4] / 2
-		b2_x2 = box2[..., 0:1] + box2[..., 2:3] / 2
-		b2_y2 = box2[..., 1:2] + box2[..., 3:4] / 2
-
-		x1 = torch.max(b1_x1, b2_x1) 
-		y1 = torch.max(b1_y1, b2_y1) 
-		x2 = torch.min(b1_x2, b2_x2) 
-		y2 = torch.min(b1_y2, b2_y2) 
-		
-		intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0) 
-
-		box1_area = abs((b1_x2 - b1_x1) * (b1_y2 - b1_y1)) 
-		box2_area = abs((b2_x2 - b2_x1) * (b2_y2 - b2_y1)) 
-		union = box1_area + box2_area - intersection 
-
-		epsilon = 1e-6
-		iou_score = intersection / (union + epsilon) 
-
-		return iou_score 
-	
-	else: 
-		intersection_area = torch.min(box1[..., 0], box2[..., 0]) * torch.min(box1[..., 1], box2[..., 1]) 
-		box1_area = box1[..., 0] * box1[..., 1] 
-		box2_area = box2[..., 0] * box2[..., 1] 
-		union_area = box1_area + box2_area - intersection_area 
-
-		iou_score = intersection_area / union_area 
-
-		return iou_score
-
 def ciou(box1, box2, mode=config.CIOU_MODE.CI0U): 
-	if mode != config.CIOU_MODE.WidthHeight: 
-		b1_x1, b1_y1, b1_x2, b1_y2 = box1[..., 0] - box1[..., 2] / 2, box1[..., 1] - box1[..., 3] / 2, box1[..., 0] + box1[..., 2] / 2, box1[..., 1] + box1[..., 3] / 2
-		b2_x1, b2_y1, b2_x2, b2_y2 = box2[..., 0] - box2[..., 2] / 2, box2[..., 1] - box2[..., 3] / 2, box2[..., 0] + box2[..., 2] / 2, box2[..., 1] + box2[..., 3] / 2
-		
-		intersection_area = torch.clamp(torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1), min=0) * torch.clamp(torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1), min=0)
-		union_area = ((b1_x2 - b1_x1) * (b1_y2 - b1_y1)) + ((b2_x2 - b2_x1) * (b2_y2 - b2_y1)) - intersection_area
-		
-		iou_score = stable_divide(intersection_area, union_area)
+    if mode != config.CIOU_MODE.WidthHeight: 
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[..., 0] - box1[..., 2] / 2, box1[..., 1] - box1[..., 3] / 2, box1[..., 0] + box1[..., 2] / 2, box1[..., 1] + box1[..., 3] / 2
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[..., 0] - box2[..., 2] / 2, box2[..., 1] - box2[..., 3] / 2, box2[..., 0] + box2[..., 2] / 2, box2[..., 1] + box2[..., 3] / 2
+        
+        intersection_area = torch.clamp(torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1), min=0) * torch.clamp(torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1), min=0)
+        union_area = ((b1_x2 - b1_x1) * (b1_y2 - b1_y1)) + ((b2_x2 - b2_x1) * (b2_y2 - b2_y1)) - intersection_area
+        
+        iou_score = stable_divide(intersection_area, union_area)
 
-		if mode == config.CIOU_MODE.IoU:
-			return iou_score
-		
-		# Center distance
-		center_distance = ((box1[..., 0] - box2[..., 0])**2 + (box1[..., 1] - box2[..., 1])**2)
-		
-		# Enclosing box
-		c_x1, c_y1, c_x2, c_y2 = torch.min(b1_x1, b2_x1), torch.min(b1_y1, b2_y1), torch.max(b1_x2, b2_x2), torch.max(b1_y2, b2_y2)
-		c_diag = ((c_x2 - c_x1)**2 + (c_y2 - c_y1)**2)
-		
-		# Aspect ratio
-		v = stable_divide(4, np.pi ** 2) * ((torch.atan(box1[..., 2] / box1[..., 3]) - torch.atan(box2[..., 2] / box2[..., 3])) ** 2)
-		alpha = stable_divide(v, 1 - iou_score + v)
+        if mode == config.CIOU_MODE.IoU:
+            return iou_score
+        
+        # Center distance
+        center_distance = ((box1[..., 0] - box2[..., 0])**2 + (box1[..., 1] - box2[..., 1])**2)
+        
+        # Enclosing box
+        c_x1, c_y1, c_x2, c_y2 = torch.min(b1_x1, b2_x1), torch.min(b1_y1, b2_y1), torch.max(b1_x2, b2_x2), torch.max(b1_y2, b2_y2)
+        c_diag = ((c_x2 - c_x1)**2 + (c_y2 - c_y1)**2)
+        
+        # Aspect ratio
+        v = stable_divide(4, np.pi ** 2) * ((torch.atan(box1[..., 2] / box1[..., 3]) - torch.atan(box2[..., 2] / box2[..., 3])) ** 2)
+        alpha = stable_divide(v, 1 - iou_score + v)
 
-		ciou_score = iou_score - stable_divide(center_distance, c_diag) - (alpha * v)
+        ciou_score = iou_score - stable_divide(center_distance, c_diag) - (alpha * v)
 
-		return ciou_score.unsqueeze(1)
-	else: 
-		intersection_area = torch.min(box1[..., 0], box2[..., 0]) * torch.min(box1[..., 1], box2[..., 1]) 
+        return ciou_score.unsqueeze(1)
+    else: 
+        intersection_area = torch.min(box1[..., 0], box2[..., 0]) * torch.min(box1[..., 1], box2[..., 1]) 
 
-		box1_area = box1[..., 0] * box1[..., 1] 
-		box2_area = box2[..., 0] * box2[..., 1] 
-		union_area = box1_area + box2_area - intersection_area 
-		iou_score = stable_divide(intersection_area, union_area)
+        box1_area = box1[..., 0] * box1[..., 1] 
+        box2_area = box2[..., 0] * box2[..., 1] 
+        union_area = box1_area + box2_area - intersection_area 
+        iou_score = stable_divide(intersection_area, union_area)
 
-		return iou_score
+        return iou_score
 
 def convert_cells_to_bboxes(predictions, scaled_anchors, grid_size, to_list=True): 
-	batch_size = predictions.shape[0] 
-	num_anchors = len(scaled_anchors) 
-	scaled_anchors = scaled_anchors.reshape(1, num_anchors, 1, 1, 2) 
-	
-	box_predictions = torch.cat([torch.sigmoid(predictions[..., 1:3] ), 
+    batch_size = predictions.shape[0] 
+    num_anchors = len(scaled_anchors) 
+    scaled_anchors = scaled_anchors.reshape(1, num_anchors, 1, 1, 2) 
+    
+    box_predictions = torch.cat([torch.sigmoid(predictions[..., 1:3] ), 
                                torch.exp(predictions[..., 3:5] ) * scaled_anchors 
                             ],dim=-1) 
-	
-	objectness = torch.sigmoid(predictions[..., 0:1]) 
-	best_class = torch.argmax(predictions[..., 5:], dim=-1).unsqueeze(-1) 
-	
-	# Calculate cell indices 
-	cell_indices = ( 
-		torch.arange(grid_size) 
-		.repeat(predictions.shape[0], 3, grid_size, 1) 
-		.unsqueeze(-1) 
-		.to(predictions.device) 
-	) 
+    
+    objectness = torch.sigmoid(predictions[..., 0:1]) 
+    best_class = torch.argmax(predictions[..., 5:], dim=-1).unsqueeze(-1) 
+    
+    # Calculate cell indices 
+    cell_indices = ( 
+        torch.arange(grid_size) 
+        .repeat(predictions.shape[0], 3, grid_size, 1) 
+        .unsqueeze(-1) 
+        .to(predictions.device) 
+    ) 
 
-	scale_multiplier = 1 / grid_size
-	x = scale_multiplier * (box_predictions[..., 0:1] + cell_indices) 
-	y = scale_multiplier * (box_predictions[..., 1:2] + cell_indices.permute(0, 1, 3, 2, 4)) 
-	width = scale_multiplier * box_predictions[..., 2:3] 
-	height = scale_multiplier * box_predictions[..., 3:4] 
+    scale_multiplier = 1 / grid_size
+    x = scale_multiplier * (box_predictions[..., 0:1] + cell_indices) 
+    y = scale_multiplier * (box_predictions[..., 1:2] + cell_indices.permute(0, 1, 3, 2, 4)) 
+    width = scale_multiplier * box_predictions[..., 2:3] 
+    height = scale_multiplier * box_predictions[..., 3:4] 
 
-	converted_bboxes = torch.cat((objectness, x, y, width, height, best_class), dim=-1)
+    converted_bboxes = torch.cat((objectness, x, y, width, height, best_class), dim=-1)
 
-	return converted_bboxes if not to_list else converted_bboxes.reshape(
-		batch_size, num_anchors * grid_size * grid_size, 6).tolist()
+    return converted_bboxes if not to_list else converted_bboxes.reshape(
+        batch_size, num_anchors * grid_size * grid_size, 6).tolist()
 
 def nms(bboxes, enough_overlap_threshold, valid_prediction_threshold):
-	# Check decodePrediction method for why objectness is stored at index 0
+    # Check decodePrediction method for why objectness is stored at index 0
     bboxes = [box for box in bboxes if box[0] > valid_prediction_threshold]
     bboxes = sorted(bboxes, key=lambda x: x[0], reverse=True)
     bboxes_nms = []
+    counter = 0
+
     while bboxes:
         first_box = bboxes.pop(0)
         bboxes_nms.append(first_box)
 
+        print(counter)
+        counter+=1
+
         # Keep only bounding boxes that do not overlap significantly with the first_box  
-		# And skip for different classes, because prediction for different classes should be independent
-		# Check decodePrediction for why class_prediction is stored at index 5 and why bbox parameter is stored at index [1:5]
-        bboxes = [box for box in bboxes if box[5] != first_box[5] or iou(torch.tensor(first_box[1:5]), torch.tensor(box[1:5])) < enough_overlap_threshold]
+        # And skip for different classes, because prediction for different classes should be independent
+        # Check decodePrediction for why class_prediction is stored at index 5 and why bbox parameter is stored at index [1:5]
+        bboxes = [box for box in bboxes if box[5] != first_box[5] or ciou(torch.tensor(first_box[1:5]), torch.tensor(box[1:5]), mode=config.CIOU_MODE.IoU) < enough_overlap_threshold]
 
     return bboxes_nms
 
 def plot_image(image, boxes): 
-	colour_map = plt.get_cmap("tab20b") 
-	colors = [colour_map(i) for i in np.linspace(0, 1, config.num_classes)] 
+    colour_map = plt.get_cmap("tab20b") 
+    colors = [colour_map(i) for i in np.linspace(0, 1, config.num_classes)] 
 
-	img = np.array(image) 
-	h, w, _ = img.shape 
-	fig, ax = plt.subplots(1) 
-	ax.imshow(img) 
+    img = np.array(image) 
+    h, w, _ = img.shape 
+    fig, ax = plt.subplots(1) 
+    ax.imshow(img) 
 
-	for box in boxes: 
-		class_pred = box[5] 
-		box = box[1:5] 
-		upper_left_x = box[0] - box[2] / 2
-		upper_left_y = box[1] - box[3] / 2
+    for box in boxes: 
+        class_pred = box[5] 
+        box = box[1:5] 
+        upper_left_x = box[0] - box[2] / 2
+        upper_left_y = box[1] - box[3] / 2
 
-		rect = patches.Rectangle( 
-			(upper_left_x * w, upper_left_y * h), 
-			box[2] * w, 
-			box[3] * h, 
-			linewidth=2, 
-			edgecolor=colors[int(class_pred)], 
-			facecolor="none", 
-		) 
-		
-		ax.add_patch(rect) 
-		plt.text( 
-			upper_left_x * w, 
-			upper_left_y * h, 
-			s=config.class_labels[int(class_pred)], 
-			color="white", 
-			verticalalignment="top", 
-			bbox={"color": colors[int(class_pred)], "pad": 0}, 
-		) 
+        rect = patches.Rectangle( 
+            (upper_left_x * w, upper_left_y * h), 
+            box[2] * w, 
+            box[3] * h, 
+            linewidth=2, 
+            edgecolor=colors[int(class_pred)], 
+            facecolor="none", 
+        ) 
+        
+        ax.add_patch(rect) 
+        plt.text( 
+            upper_left_x * w, 
+            upper_left_y * h, 
+            s=config.class_labels[int(class_pred)], 
+            color="white", 
+            verticalalignment="top", 
+            bbox={"color": colors[int(class_pred)], "pad": 0}, 
+        ) 
 
-	plt.show()
+    plt.show()
 
 def save_checkpoint(model, optimizer, checkpoint_file): 
-	print("==> Saving checkpoint") 
-	checkpoint = { 
-		"state_dict": model.state_dict(), 
-		"optimizer": optimizer.state_dict(), 
-	} 
-	torch.save(checkpoint, checkpoint_file)
+    print("==> Saving checkpoint") 
+    checkpoint = { 
+        "state_dict": model.state_dict(), 
+        "optimizer": optimizer.state_dict(), 
+    } 
+    torch.save(checkpoint, checkpoint_file)
 
 def load_checkpoint(checkpoint_file, model, optimizer, lr): 
     if not os.path.exists(checkpoint_file):
@@ -186,4 +153,4 @@ def load_checkpoint(checkpoint_file, model, optimizer, lr):
         param_group["lr"] = lr
 
 def stable_divide(a, b):
-	return a / (b + config.numerical_stability)
+    return a / (b + config.numerical_stability)
