930c83b070e355be104f3319e4c103f8a8394aae
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-04 21:41:24 -0600

    1

--- v5/config.py
+++ v5/config.py
@@ -3,6 +3,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2 
 import cv2 
 import os
+from kmeans_anchor import auto_anchor
 
 # -------------------------------------- DATASET --------------------------------------
 class_labels = [ 
@@ -24,8 +25,8 @@ if os.name == 'nt':
 	label_dir = os.path.join(base_dir, "data", dataset, "labels")
 	checkpoint_file = os.path.join(base_dir, "v5", f"{dataset}_checkpoint.pth.tar")
 else:
-	train_csv_file = f"../data/{dataset}/2examples.csv"
-	test_csv_file = f"../data/{dataset}/2examples_test.csv"
+	train_csv_file = f"../data/{dataset}/8examples.csv"
+	test_csv_file = f"../data/{dataset}/8examples_test.csv"
 	#train_csv_file = f"../data/{dataset}/train.csv"
 	#test_csv_file = f"../data/{dataset}/test.csv"
 	image_dir = f"../data/{dataset}/images/"
@@ -44,13 +45,15 @@ enough_overlap_threshold = 0.6
 valid_prediction_threshold = 0.6
 
 image_size = 416
-# ANCHORS = auto_anchor(self.num_anchors, self.label_dir, self.s)
-ANCHORS = [ 
-	[(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)], 
-	[(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)], 
-	[(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)], 
-] 
 grid_sizes = [image_size // 32, image_size // 16, image_size // 8] 
+num_anchors = 3
+
+ANCHORS = auto_anchor(num_anchors, label_dir, grid_sizes)
+# ANCHORS = [ 
+# 	[(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)], 
+# 	[(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)], 
+# 	[(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)], 
+# ] 
 scaled_anchors = ( 
                 torch.tensor(ANCHORS) *
                 torch.tensor(grid_sizes).unsqueeze(1).unsqueeze(1).repeat(1,3,2) 
