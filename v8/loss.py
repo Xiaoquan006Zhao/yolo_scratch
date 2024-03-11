0791eac2ed0cfdb587b49453ed350b1af35301fc
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-10 23:04:31 -0500

    clean up

--- v8/loss.py
+++ v8/loss.py
@@ -13,20 +13,15 @@ class YOLOLoss(nn.Module):
 		self.sigmoid = nn.Sigmoid() 
 	
 	def forward(self, pred, target, anchors): 
+		anchors = anchors.reshape(1, 3, 1, 1, 2) 
+
 		obj = target[..., 0] == 1
 		no_obj = target[..., 0] == 0
 
-		no_object_loss = self.bce( 
-			(pred[..., 0:1][no_obj]), (target[..., 0:1][no_obj]), 
-		) 
-
-		anchors = anchors.reshape(1, 3, 1, 1, 2) 
+		no_object_loss = self.bce((pred[..., 0:1][no_obj]), (target[..., 0:1][no_obj]), ) 
 
 		box_preds = torch.cat([self.sigmoid(pred[..., 1:3]), torch.exp(pred[..., 3:5]) * anchors],dim=-1)
-
 		ious = ciou(box_preds[obj], target[..., 1:5][obj])
-
-
 		# The way I understand why ious*target is that since the objectiveness and bbox is
 		# tied together. If the bbox has small or no overlap, we should discard it's objectiveness
 		# prediction as well and penalize the model more for confident objectiveness prediction
@@ -34,9 +29,7 @@ class YOLOLoss(nn.Module):
 		object_loss = self.mse(self.sigmoid(pred[..., 0:1][obj]), ious * target[..., 0:1][obj]) 
 		
 		pred[..., 1:3] = self.sigmoid(pred[..., 1:3]) 
-		
 		target[..., 3:5] = torch.log(1e-6 + target[..., 3:5] / anchors) 
-
 		box_loss = self.mse(pred[..., 1:5][obj], target[..., 1:5][obj]) 
 
 		class_loss = self.cross_entropy((pred[..., 5:][obj]), target[..., 5][obj].long()) 
