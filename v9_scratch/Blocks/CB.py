c52accae02ec57273416c02647893a042b80c7da
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-14 06:51:34 -0500

    6

--- v9_scratch/Blocks/CB.py
+++ v9_scratch/Blocks/CB.py
@@ -18,6 +18,7 @@ class CBLinear(nn.Module):
     def forward(self, xs):
         x = xs[self.route_connection]
         self.conv = nn.Conv2d(x.shape[1], sum(self.c2s), self.k, self.s, autopad(self.k, self.p), groups=self.g, bias=True)
+        self.conv.bias.data = self.conv.bias.data.to(x.dtype)
         outs = self.conv(x).split(self.c2s, dim=1)
         return outs
 
