2e3eb45861be6e3dda7eb553539e44e54c2cd453
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-14 23:16:42 -0500

    Revert "8"
    This reverts commit 43a00c9304a9233f21f8c62994212b8ef8df698e.

--- v9_scratch/model.py
+++ v9_scratch/model.py
@@ -1,7 +1,5 @@
 import torch.nn as nn 
 import config
-from Blocks.CSP import CSPBlock
-from Blocks.PAN import PAN
 from Blocks.BasicBlock import (
     ConvBNMish,
     Conv,
@@ -14,7 +12,6 @@ from Blocks.RepNCSPELAN4 import (
     RepNCSPELAN4,
 )
 from Blocks.ScaledPredictions import (
-    ScaledPrediction,
     ScaledPredictions,
 )
 from Blocks.CB import (
@@ -22,35 +19,39 @@ from Blocks.CB import (
     CBFuse,
 )
 from Blocks.SPPELAN import (
-    SPPFBlock,
     SPPELAN,
 )
 
 class YOLOv9(nn.Module): 
     def __init__(self, in_channels=3, num_classes=20, TRAINING=True): 
         super().__init__() 
+        self.TRAINING = TRAINING
         self.num_classes = num_classes 
         self.in_channels = in_channels 
-        
+        self.layer_outputs = []
 
-        self.layers = nn.ModuleList([ 
+        self.inference_layers = nn.ModuleList([ 
             Silence(),
             Conv(self.in_channels, 64, k=3, s=2), 
             Conv(64, 128, k=3, s=2), 
 
-            RepNCSPELAN4(128, 256, 128, 64),
+            Conv(128, 256), 
+            # RepNCSPELAN4(128, 256, 128, 64),
 
             Conv(256, 256, k=3, s=2), 
 
-            RepNCSPELAN4(256, 512, 256, 128),
+            Conv(256, 512), 
+            # RepNCSPELAN4(256, 512, 256, 128),
 
             Conv(512, 512, k=3, s=2), 
 
-            RepNCSPELAN4(512, 512, 512, 256),
+            Conv(512, 512), 
+            # RepNCSPELAN4(512, 512, 512, 256),
 
             Conv(512, 512, k=3, s=2), 
 
-            RepNCSPELAN4(512, 512, 512, 256),
+            Conv(512, 512), 
+            # RepNCSPELAN4(512, 512, 512, 256),
             
             # --------------------------
             SPPELAN(512, 512, 256),
@@ -58,52 +59,80 @@ class YOLOv9(nn.Module):
             Upsample(),
             Concat([7, -1], 1),
 
-            RepNCSPELAN4(1024, 512, 512, 256),
+            Conv(1024, 512), 
+            # RepNCSPELAN4(1024, 512, 512, 256),
 
             Upsample(),
             Concat([5, -1], 1),
 
-            RepNCSPELAN4(1024, 256, 256, 128),
+            Conv(1024, 256), 
+            # RepNCSPELAN4(1024, 256, 256, 128),
 
             Downsample(256),
             Concat([13, -1], 1),
 
-            RepNCSPELAN4(768, 512, 512, 256),
+            Conv(768, 512), 
+            # RepNCSPELAN4(768, 512, 512, 256),
 
             Downsample(512),
             Concat([10, -1], 1),
 
-            RepNCSPELAN4(1024, 512, 512, 256),
-            
+            Conv(1024, 512), 
+            # RepNCSPELAN4(1024, 512, 512, 256),
+        ]) 
+
+        self.inference_prediction = nn.ModuleList([
             ScaledPredictions([16, 19, 22], [256, 512, 512], self.num_classes)
+        ])
+
+        self.auxiliary_layers = nn.ModuleList([ 
+            CBLinear([5], 512, [256]),
+            CBLinear([7], 512, [256, 512]),
+            CBLinear([9], 512, [256, 512, 512]),
+
+            Conv(self.in_channels, 64, k=3, s=2), 
+            Conv(64, 128, k=3, s=2), 
+            RepNCSPELAN4(128, 256, 128, 64),
+
+            Conv(256, 256, k=3, s=2), 
+
+            CBFuse([23, 24, 25, -1], [0,0,0]),
+            RepNCSPELAN4(256, 512, 256, 128),
+
+            Conv(512, 512, k=3, s=2), 
+            CBFuse([24, 25, -1], [1,1]),
+            RepNCSPELAN4(512, 512, 512, 256),
+
+            Conv(512, 512, k=3, s=2), 
+            CBFuse([25, -1], [2]),
+            RepNCSPELAN4(512, 512, 512, 256),
+
+            ScaledPredictions([31, 34, 37, 16, 19, 22], [512, 512, 512, 256, 512, 512], self.num_classes)
         ]) 
     
     def forward(self, x): 
-        # if self.TRAINING:
-        #     self.layers = self.inference_layers + self.auxiliary_layers
-        # else:
-        #     self.layers = self.inference_layers + self.inference_prediction
-
-        layer_outputs = []
+        if self.TRAINING:
+            self.layers = self.inference_layers + self.auxiliary_layers
+        else:
+            self.layers = self.inference_layers + self.inference_prediction
 
         for layer in self.layers:
             if isinstance(layer, ScaledPredictions):
                 route_list = layer.route_list
-                selected_tensors = [layer_outputs[i] for i in route_list]
+                selected_tensors = [self.layer_outputs[i] for i in route_list]
 
                 predictions = layer(selected_tensors)
                 return predictions
             elif isinstance(layer, (CBFuse, CBLinear, Concat)):
                 route_list = layer.route_list
-                selected_tensors = [layer_outputs[i] for i in route_list]
+                selected_tensors = [self.layer_outputs[i] for i in route_list]
 
                 x = layer(selected_tensors)
             else:
                 x = layer(x)
 
-            layer_outputs.append(x)
+            self.layer_outputs.append(x)
 
-            # if isinstance(layer, CBLinear):
-            #     x = self.layer_outputs[0]
-
+            if isinstance(layer, CBLinear):
+                x = self.layer_outputs[0]
 
