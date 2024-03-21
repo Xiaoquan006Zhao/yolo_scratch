b2f01f0a66a5ebf8ac9e043b32c505c9239e6090
Xiaoquan006Zhao <1009642031@qq.com>
2024-03-21 13:58:07 -0500

    v8 revert

--- v8/model.py
+++ v8/model.py
@@ -14,14 +14,12 @@ class YOLOv8(nn.Module):
 		self.layers = nn.ModuleList([ 
 			ConvBNMish(in_channels, 64, kernel_size=3, stride=2, padding=1), 
 			ConvBNMish(64, 128, kernel_size=3, stride=2, padding=1), 
-			CSPBlock(128, 128, bottleNeck_use_residual=True, BottleNeck_repeats=3),
 
+			CSPBlock(128, 128, bottleNeck_use_residual=True, BottleNeck_repeats=3),
 			ConvBNMish(128, 256, kernel_size=3, stride=2, padding=1), 
 			CSPBlock(256, 256, bottleNeck_use_residual=True, BottleNeck_repeats=6),
-
 			ConvBNMish(256, 512, kernel_size=3, stride=2, padding=1), 
 			CSPBlock(512, 512, bottleNeck_use_residual=True, BottleNeck_repeats=9),
-
 			ConvBNMish(512, 1024, kernel_size=3, stride=2, padding=1), 
 			CSPBlock(1024, 1024, bottleNeck_use_residual=True, BottleNeck_repeats=4),
 
@@ -43,7 +41,7 @@ class YOLOv8(nn.Module):
 
 			if isinstance(layer, SPPFBlock): 
 				route_connections.append(x) 
-
+				
 		# return outputs
 
 
